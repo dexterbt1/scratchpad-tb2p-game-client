87ed601fe6d67e0632a0e5548aa7d9a924e4f963
dexter@dexter-portable <dexter@dexter-portable>
2009-09-26 01:00:09 +0800

    - initial commit - refactored and branched from playhub-server

---
+++ src/playhub/tb2p/exceptions/GameStateViolation.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package playhub.tb2p.exceptions;
+
+/**
+ *
+ * @author dexter
+ */
+public class GameStateViolation extends Exception {
+
+}
---
+++ src/playhub/tb2p/exceptions/MalformedPDUException.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package playhub.tb2p.exceptions;
+
+/**
+ *
+ * @author dexter
+ */
+public class MalformedPDUException extends Exception {
+
+    public MalformedPDUException(String s) { super(s); }
+
+}
